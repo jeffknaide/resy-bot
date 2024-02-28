613152351d7a306c655d1267d0a9f7cd2b7413b2
Chris Fischer <chris@fischer.nyc>
2024-02-27 21:06:12 -0500

    added a timzezone to TZ-scheduler

--- resy_bot/scheduler.py
+++ resy_bot/scheduler.py
@@ -4,6 +4,6 @@ from resy_bot.logging import logging
 def initialize():
     logger = logging.getLogger("Scheduler")
     logger.setLevel("INFO")
-    scheduler = BackgroundScheduler(logger=logger)
+    scheduler = BackgroundScheduler(logger=logger, timezone='America/New_York')
     scheduler.start()
     return scheduler
